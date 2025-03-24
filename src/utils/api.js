f49b032115d85173d3f08e3fce6fab61a0e547e6
RomuloBryner <romulobry@gmail.com>
2025-03-24 05:53:18 -0400

    Added Environment variable

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:1337/api";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337/api";
 
 export const getSolicitudes = async () => {
   const response = await axios.get(`${API_BASE}/solicituds?populate=*`);
