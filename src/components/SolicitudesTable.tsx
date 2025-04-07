bf8e3022eaba8c3d0debb3da8503fbcd4275b96e
RomuloBryner <romulobry@gmail.com>
2025-04-07 13:14:36 -0400

    Fix, error 2

--- src/components/SolicitudesTable.tsx
+++ src/components/SolicitudesTable.tsx
@@ -80,7 +80,6 @@ export function RequestsTable({ className }: { className?: string }) {
     setCurrentRequest(id);
     setFormData({
       responsible: request?.responsible || "",
-      limit_date: request?.limit_date || "",
       progress: request?.progress || "",
     });
     setUpdateModalOpen(true);
